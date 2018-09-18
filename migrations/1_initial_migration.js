58a6d93d7608219dbcea6c8753ff4ae41817fb11
Jaanek Oja <jaanek@oja.me>
2018-09-18 15:58:42 +0300

    Ropsten deploy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
+  console.log(`Accounts:`, accounts);
   deployer.deploy(Migrations);
 };
--- migrations/2_migration.js
+++ migrations/2_migration.js
@@ -4,16 +4,28 @@ const Whitelist = artifacts.require('Whitelist')
 const ChannelOperator = artifacts.require('ChannelOperator')
 const EIP820Registry = require("eip820")
 
+const truffleConfig = require('../truffle.js');
+
 module.exports = function (deployer, network, accounts) {
-    deployer.deploy(Whitelist).then(() => {
-        if (network === 'development') {
-            const Web3Latest = require("web3")
-            const web3latest = new Web3Latest('http://localhost:8545')
-            return EIP820Registry.deploy(web3latest, accounts[0])
-        } else {
-            return null;
-        }
-    })
+  console.log(`Accounts:`, accounts);
+  const provider = truffleConfig.networks[network].provider();
+  console.log(`Provider url:`, provider.url);
+
+  let providerUrl = 'http://localhost:8545';
+  if (provider && provider.url) {
+    providerUrl = provider.url;
+  }
+
+  deployer.deploy(Whitelist).then(() => {
+    if (network === 'development' || network === 'ropsten') {
+      console.log(`Deploying EIP820 registry`);
+      const Web3Latest = require("web3")
+      const web3latest = new Web3Latest(providerUrl)
+      return EIP820Registry.deploy(web3latest, accounts[0])
+    } else {
+      return null;
+    }
+  })
     .then(() => deployer.deploy(NOIAToken))
     .then(() => deployer.deploy(NOIACrowdsale, NOIAToken.address, Whitelist.address))
     .then(() => deployer.deploy(ChannelOperator, NOIAToken.address))
